6563b2e34e498d1478b3be916cadc690506ae858
Henry Arevalo <138257615+HenryArevaloBarrera@users.noreply.github.com>
2026-05-20 22:34:31 -0500

    Delete product-service directory

--- product-service/src/main/java/DIMENSITY/SOFTWARE/Back_Catherino/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package DIMENSITY.SOFTWARE.Back_Catherino.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import DIMENSITY.SOFTWARE.Back_Catherino.model.Product;
-
-@Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
